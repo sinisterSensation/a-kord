f33216dacfb9ea2ce2630f5e45ee9c2cb7b679e7
sinisterSensation <bayukov.kirya@mail.ru>
2024-06-20 01:44:56 +0300

    basket

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, Container } from "react-bootstrap";
+import { Button, Card, Container, Image } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
 
 const Basket = observer(() => {
@@ -19,31 +19,64 @@ const Basket = observer(() => {
 	return (
 		<Container
 			className="d-flex flex-sm-column justify-content-center align-items-center mt-5 mb-5"
-			style={{ height: 650 }}
+			style={{ height: window.innerHeight - 280, overflowY: "auto", overflowX: "hidden" }}
 		>
 			<h1 className="pb-2">Корзина</h1>
-			{cartItems.map((item) => (
-				<Card
-					key={item.id}
-					className="d-flex flex-row  p-2 justify-content-between align-items-center mb-2 mt-5"
-				>
-					<h1 className="pr-2">{item.name}</h1>
-					<h3 className="pl-2">
-						{item.price}
-						<span className="font-weight-light pl-2">рублей</span>
-					</h3>
-					<button onClick={() => removeItem(item.id)}>Удалить</button>
-				</Card>
-			))}
-			<Card className="d-flex flex-row  p-2 justify-content-between align-items-center mb-2 mt-5">
-				<h1 className="pr-2">Итого:</h1>
-				<h3 className="pl-2">
-					{prices}
-					<span className="font-weight-light pl-2">рублей</span>
-				</h3>
-			</Card>
+			{cartItems.length === 0 ? (
+				<h3>пуста</h3>
+			) : (
+				<>
+					<div style={{ maxHeight: "500px", overflowY: "auto" }}>
+						{cartItems.map((item) => (
+							<Card
+								key={item.id}
+								style={{ background: "#323338", borderRadius: "7px", color: "white", width: "55rem" }}
+								className="mt-5 d-flex justify-content-around align-items-center flex-row p-4"
+							>
+								<Image
+									src={process.env.REACT_APP_API_URL + item.img}
+									width={150}
+									height={150}
+									style={{ borderRadius: 5 }}
+								/>
+								<div className="pr-2" style={{ fontSize: "20px" }}>
+									{item.name}
+								</div>
+								<div className="pl-2" style={{ fontSize: "20px" }}>
+									{item.price}
+									<span className="font-weight-light pl-2"> ₽</span>
+								</div>
+								<Button
+									variant="outline-light"
+									style={{
+										width: "100px",
+										boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+										background: "#5C5C5C",
+										border: "0.5px solid #FFF",
+										borderRadius: "7px",
+									}}
+									onClick={() => removeItem(item.id)}
+								>
+									Удалить
+								</Button>
+							</Card>
+						))}
+					</div>
+					<Card
+						className="d-flex flex-row  p-1 justify-content-between align-items-center mb-2 mt-5"
+						style={{ color: "white", backgroundColor: "#323338" }}
+					>
+						<h1 className="pr-2 m-2">Итого:</h1>
+						<h3 className="pl-2 m-2">
+							{prices}
+							<span className="font-weight-light pl-2 m-1"> ₽</span>
+						</h3>
+					</Card>
+				</>
+			)}
 		</Container>
 	);
 });
 
 export default Basket;
+
